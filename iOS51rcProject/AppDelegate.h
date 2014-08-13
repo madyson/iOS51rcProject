af38e46acfac81c4d59b0f4bfab756b06f577352
Lucifer <lucifer@LuciferdeMacBook-Air.local>
2014-08-13 16:55:30 +0800

    Initial Commit

---
+++ iOS51rcProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS51rcProject
+//
+//  Created by Lucifer on 14-8-13.
+//  Copyright (c) 2014年 Lucifer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
