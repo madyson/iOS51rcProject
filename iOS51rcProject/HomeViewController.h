bc3e9de7a964f6722e99985cb5dbd412c20b261f
qlrc <Lucifer@oukaixintekiMac-mini.local>
2014-08-20 09:32:35 +0800

    完成home到登录页面的导航

---
+++ iOS51rcProject/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  iOS51rcProject
+//
+//  Created by qlrc on 14-8-20.
+//  Copyright (c) 2014年 Lucifer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
