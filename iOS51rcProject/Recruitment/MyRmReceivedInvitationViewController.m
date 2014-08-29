de1b4d9103877dfbbd9021665573327fd7dbf38d
qlrc <Lucifer@oukaixintekiMac-mini.local>
2014-08-29 10:43:13 +0800

    完成招聘会收到的邀请页面

--- iOS51rcProject/Recruitment/MyRmReceivedInvitationViewController.m
+++ iOS51rcProject/Recruitment/MyRmReceivedInvitationViewController.m
@@ -36,11 +36,11 @@
 {
     [super viewDidLoad];
     selectRowIndex = 0;
+    selectRowHeight = 110;//选择行的高度
     
     //数据加载等待控件初始化
     loadView = [[LoadingAnimationView alloc] initWithFrame:CGRectMake(140, 100, 80, 98) loadingAnimationViewStyle:LoadingAnimationViewStyleCarton target:self];
     [self onSearch];
-    
 }
 
 - (void)onSearch
@@ -52,7 +52,7 @@
     NSString *code = [userDefaults objectForKey:@"code"];
     NSString *userID = [userDefaults objectForKey:@"UserID"];
     NSMutableDictionary *dicParam = [[NSMutableDictionary alloc] init];
-    [dicParam setObject:userID forKey:@"paMainID"];//
+    [dicParam setObject:userID forKey:@"paMainID"];//21142013
     [dicParam setObject:code forKey:@"code"];//152014391908
     NetWebServiceRequest *request = [NetWebServiceRequest serviceRequestUrl:@"GetMyReceivedInvitationList" Params:dicParam];
     [request setDelegate:self];
@@ -81,11 +81,30 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [[[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleSubtitle) reuseIdentifier:@"cpList"] autorelease];
     //主要信息
-    UIView *viewJobMain;
+    //UIView *viewJobMain;
     NSDictionary *rowData = recruitmentCpData[indexPath.row];
-    //标题左侧的红线
+    
+    //是否已经结束
+    BOOL isPassed = false;
+    NSString *strEndDate = rowData[@"EndDate"];
+    NSDate *dtEndDate = [CommonController dateFromString:strEndDate];
+    NSDate *earlierDate =  [dtEndDate earlierDate:[NSDate date]];//与当前时间比较
+    if (earlierDate != dtEndDate) {
+        isPassed = false;
+    }else{
+        isPassed = true;
+    }
+    //操作状态
+    NSString *strStatus = rowData[@"Status"];
+    
+    //标题左侧的红线(已经处理则显示灰色)
     UILabel *lbLeft = [[UILabel alloc] initWithFrame:CGRectMake(0, 4, 5, 20)];
-    lbLeft.layer.backgroundColor = [UIColor colorWithRed:255/255.0 green:90/255.0 blue:49/255.0 alpha:1].CGColor;
+    if (![strStatus isEqualToString:@"0"]) {
+        lbLeft.layer.backgroundColor = [UIColor colorWithRed:255/255.0 green:90/255.0 blue:49/255.0 alpha:1].CGColor;
+    }else{
+        lbLeft.layer.backgroundColor = [UIColor grayColor].CGColor;
+    }
+    
     [cell.contentView addSubview:lbLeft];
     [lbLeft release];
     //职位标题
@@ -109,6 +128,23 @@
     [cell.contentView addSubview:btnChat];
     [btnChat release];
     [imgOnline release];
+    //右侧的参会不参会标记
+    UILabel *lbStatus = [[UILabel alloc] initWithFrame:CGRectMake(260, 6, 50, 18)];
+    lbStatus.layer.cornerRadius = 7;
+    lbStatus.textColor = [UIColor whiteColor];
+    lbStatus.font = [UIFont systemFontOfSize:12];
+    lbStatus.textAlignment = NSTextAlignmentCenter;
+    if (!isPassed) {
+        if ([strStatus isEqualToString:@"1"]) {//参会
+            lbStatus.layer.backgroundColor = [UIColor colorWithRed:3/255.0 green:187/255.0 blue:34/255.0 alpha:1].CGColor;
+            lbStatus.text = @"参会";
+        }else{
+            lbStatus.layer.backgroundColor = [UIColor grayColor].CGColor;
+            lbStatus.text = @"不参会";
+        }
+    }
+    [cell.contentView addSubview:lbStatus];
+    [lbStatus release];
     //公司名称
     NSString *strCpName = rowData[@"companyName"];
     labelSize = [CommonController CalculateFrame:strCpName fontDemond:[UIFont systemFontOfSize:11] sizeDemand:CGSizeMake(200, 15)];
@@ -130,14 +166,15 @@
     [cell.contentView addSubview:(lbInviteTime)];
     [lbInviteTime release];
     //分隔线
-    UILabel *lbLine1 = [[UILabel alloc] initWithFrame:CGRectMake(20, lbInviteTime.frame.origin.y+lbInviteTime.frame.size.height + 5, 280, 1)];
+    UILabel *lbLine1 = [[UILabel alloc] initWithFrame:CGRectMake(20, lbInviteTime.frame.origin.y+lbInviteTime.frame.size.height + 5, 300, 1)];
     lbLine1.text = @"--------------------------------------------------------------------------";
+    lbLine1.textColor = [UIColor lightGrayColor];
     [cell.contentView addSubview:lbLine1];
     [lbLine1 release];
     //招聘会名称
     NSString *strRmName = rowData[@"RecruitmentName"];
     labelSize = [CommonController CalculateFrame:strRmName fontDemond:[UIFont systemFontOfSize:11] sizeDemand:CGSizeMake(200, 15)];
-    UILabel *lbRmName = [[UILabel alloc] initWithFrame:CGRectMake(20, lbInviteTime.frame.origin.y + 20, labelSize.width, 15)];
+    UILabel *lbRmName = [[UILabel alloc] initWithFrame:CGRectMake(20, lbInviteTime.frame.origin.y + 23, labelSize.width, 15)];
     lbRmName.text = strRmName;
     lbRmName.font = [UIFont systemFontOfSize:11];
     lbRmName.textColor = [UIColor grayColor];
@@ -170,7 +207,7 @@
         UIButton *btnLngLat = [[UIButton alloc] initWithFrame:CGRectMake(20 + lbPlace.frame.size.width, lbPlace.frame.origin.y, 15, 15)];
         //NSString *lng = rowData[@"lng"];
         //NSString *lat = rowData[@"lat"];
-        UIImageView *imgLngLat = [[UIImageView alloc] initWithFrame:CGRectMake(20 + lbPlace.frame.size.width, lbPlace.frame.origin.y, 15, 15)];
+        UIImageView *imgLngLat = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 15, 15)];
         imgLngLat.image = [UIImage imageNamed:@"ico_coordinate_red.png"];
         [btnLngLat addSubview:imgLngLat];
         btnLngLat.tag = (NSInteger)rowData[@"ID"];
@@ -180,7 +217,11 @@
         [imgLngLat release];
         //展位号
         UILabel *lbDeskNo = [[UILabel alloc] initWithFrame:CGRectMake(20, lbPlace.frame.origin.y + lbPlace.frame.size.height + 5, titleWidth, 15)];
-        lbDeskNo.text = [NSString stringWithFormat:@"展 位号：%@",rowData[@"DeskNo"]];
+        NSString *strDeskNo = rowData[@"DeskNo"];
+        if (strDeskNo == nil) {
+            strDeskNo = @"";
+        }
+        lbDeskNo.text = [NSString stringWithFormat:@"展 位号：%@",strDeskNo];
         lbDeskNo.font = [UIFont systemFontOfSize:11];
         lbDeskNo.textColor = [UIColor grayColor];
         [cell.contentView addSubview:(lbDeskNo)];
@@ -204,8 +245,9 @@
         [cell.contentView addSubview:(lbXdcl)];
         [lbXdcl release];
         //分隔线2
-        UILabel *lbLine2 = [[UILabel alloc] initWithFrame:CGRectMake(20, lbXdcl.frame.origin.y+lbXdcl.frame.size.height + 5, 280, 1)];
+        UILabel *lbLine2 = [[UILabel alloc] initWithFrame:CGRectMake(20, lbXdcl.frame.origin.y+lbXdcl.frame.size.height + 5, 300, 1)];
         lbLine2.text = @"--------------------------------------------------------------------------";
+        lbLine2.textColor = [UIColor lightGrayColor];
         [cell.contentView addSubview:lbLine2];
         [lbLine2 release];
         //参会人
@@ -228,58 +270,47 @@
         [lbMobile release];
         //手机号后面的图标
         UIImageView *imgMobile = [[UIImageView alloc] initWithFrame:CGRectMake(lbMobile.frame.origin.x + lbMobile.frame.size.width, lbMobile.frame.origin.y, 15, 15)];
-        imgMobile.image = [UIImage imageNamed:@"ico_coordinate_red.png"];
-        [imgMobile addSubview:imgLngLat];
+        imgMobile.image = [UIImage imageNamed:@"ico_calltelphone.png"];
         imgMobile.tag = (NSInteger)rowData[@"ID"];
         [cell.contentView addSubview:imgMobile];
         [imgMobile release];
-        //赴约参会
-        UIButton *btnAccept = [[UIButton alloc] initWithFrame:CGRectMake(50, lbMobile.frame.origin.y + 50, 70, 30)];
-        btnAccept.tag = (NSInteger)rowData[@"ID"];
-        [btnAccept addTarget:self action:@selector(btnLngLatClick:) forControlEvents:UIControlEventTouchUpInside];
-        [cell.contentView addSubview:btnAccept];
         
-        [btnAccept release];
-        //不赴约
-        UIButton *btnReject = [[UIButton alloc] initWithFrame:CGRectMake(120, lbMobile.frame.origin.y + 50, 15, 15)];
-        btnReject.tag = (NSInteger)rowData[@"ID"];
-        [btnReject addTarget:self action:@selector(btnLngLatClick:) forControlEvents:UIControlEventTouchUpInside];
-        [cell.contentView addSubview:btnReject];
-        [btnReject release];
-        
-//        NSDictionary *rowData = recruitmentCpData[indexPath.row];//
-//        //我邀请的企业
-//        NSString *myRmCpCount = rowData[@"myInvitCpNum"] ;
-//        UIButton *btnMyRmCp = [[UIButton alloc] initWithFrame:CGRectMake(237, 58, 75, 40)];
-//        UILabel *lbMyRmCp = [[UILabel alloc] initWithFrame:CGRectMake(7, 25, 60, 10)];
-//        lbMyRmCp.text = @"我邀请的企业";
-//        lbMyRmCp.font = [UIFont systemFontOfSize:10];
-//        lbMyRmCp.textColor = [UIColor blackColor];
-//        lbMyRmCp.textAlignment = NSTextAlignmentCenter;
-//        [btnMyRmCp addSubview:lbMyRmCp];
-//        
-//        UILabel *lbMyRmCpCount = [[UILabel alloc] initWithFrame:CGRectMake(17, 9, 40, 10)];
-//        lbMyRmCpCount.text = myRmCpCount;
-//        lbMyRmCpCount.font = [UIFont systemFontOfSize:9];
-//        lbMyRmCpCount.textColor = [UIColor redColor];
-//        lbMyRmCpCount.textAlignment = NSTextAlignmentCenter;
-//        [btnMyRmCp addSubview:lbMyRmCpCount];
-//        
-//        //我邀请的企业按钮
-//        btnMyRmCp.tag = (NSInteger)rowData[@"paMainID"];
-//        btnMyRmCp.layer.borderWidth = 0.5;
-//        btnMyRmCp.layer.borderColor = [UIColor lightGrayColor].CGColor;
-//        [btnMyRmCp addTarget:self action:@selector(joinRecruitment:) forControlEvents:UIControlEventTouchUpInside];
-//        [cell.contentView addSubview:btnMyRmCp];
-//        
-//        [btnMyRmCp release];
-//        [lbMyRmCp release];
-//        [lbMyRmCpCount release];
+        //判断是否已经结束，如果没有结束，则可以赴约参会
+        if (!isPassed && [strStatus isEqualToString:@"0"]) {
+            //赴约参会
+            UIButton *btnAccept = [[UIButton alloc] initWithFrame:CGRectMake(50, lbMobile.frame.origin.y + 30, 90, 30)];
+            btnAccept.tag = (NSInteger)rowData[@"ID"];
+            [btnAccept addTarget:self action:@selector(btnLngLatClick:) forControlEvents:UIControlEventTouchUpInside];
+            btnAccept.layer.backgroundColor = [UIColor colorWithRed:3/255.0 green:187/255.0 blue:34/255.0 alpha:1].CGColor;
+            btnAccept.layer.cornerRadius = 5;
+            UILabel *lbAccept = [[UILabel alloc] initWithFrame:CGRectMake(33, 0, 99, 30)];
+            lbAccept.text = @"赴约";
+            lbAccept.textColor = [UIColor whiteColor];
+            lbAccept.font = [UIFont systemFontOfSize:12];
+            [btnAccept addSubview:lbAccept];
+            [cell.contentView addSubview:btnAccept];
+            [btnAccept release];
+            //不赴约
+            UIButton *btnReject = [[UIButton alloc] initWithFrame:CGRectMake(170, lbMobile.frame.origin.y + 30, 99, 30)];
+            btnReject.tag = (NSInteger)rowData[@"ID"];
+            [btnReject addTarget:self action:@selector(btnLngLatClick:) forControlEvents:UIControlEventTouchUpInside];
+            btnReject.layer.backgroundColor = [UIColor colorWithRed:255/255.0 green:90/255.0 blue:49/255.0 alpha:1].CGColor;
+            btnReject.layer.cornerRadius = 5;
+            UILabel *lbReject = [[UILabel alloc] initWithFrame:CGRectMake(30, 0, 99, 30)];
+            lbReject.text = @"不赴约";
+            lbReject.textColor = [UIColor whiteColor];
+            lbReject.font = [UIFont systemFontOfSize:12];
+            [btnReject addSubview:lbReject];
+            [cell.contentView addSubview:btnReject];
+            [btnReject release];
+            selectRowHeight = btnReject.frame.origin.y + btnReject.frame.size.height + 5;
+        }
+        else{
+            selectRowHeight = lbMobile.frame.origin.y + lbMobile.frame.size.height + 5;
+        }
     }else{
-        
+        selectRowHeight = 100;
     }
-    
-    
     return cell;
 }
 
@@ -287,13 +318,23 @@
     return [recruitmentCpData count];
 }
 
-//邀请企业参会
+//点击招聘会
 - (IBAction)btnInviteCp:(id)sender {
-    NSLog(@"邀请企业参会");
+    NSLog(@"");
 }
 
 //点击坐标
 -(void)btnLngLatClick:(UIButton *) sender{
+    NSLog(@"%d", sender.tag);
+}
+
+//点击参会
+-(void)btnAcceptClick:(UIButton *) sender{
+    NSLog(@"%d", sender.tag);
+}
+
+//点击不参会
+-(void)btnRejectClick:(UIButton *) sender{
     NSLog(@"%d", sender.tag);
 }
 
@@ -306,15 +347,37 @@
 //点击某一行,到达企业页面--调用代理
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [gotoRmViewDelegate gotoRmView:recruitmentCpData[indexPath.row][@"id"]];
+    selectRowIndex = indexPath.row;
+    //重新加载
+    [self.tvReceivedInvitationList reloadData];
+    [self.tvReceivedInvitationList footerEndRefreshing];
 }
 
 //每一行的高度
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (selectRowIndex == indexPath.row) {
-        return 230;
-    }else    {
-        return 110;
+    NSString *strStatus = recruitmentCpData[indexPath.row][@"Status"];
+    BOOL isPassed = false;
+    NSString *strEndDate = recruitmentCpData[indexPath.row][@"EndDate"];
+    NSDate *dtEndDate = [CommonController dateFromString:strEndDate];
+    NSDate *earlierDate =  [dtEndDate earlierDate:[NSDate date]];//与当前时间比较
+    if (earlierDate != dtEndDate) {
+        isPassed = false;
+    }else{
+        isPassed = true;
     }
+
+    if (selectRowIndex == indexPath.row) {
+        //如果未结束，并且没操作
+        if (!isPassed&&[strStatus isEqualToString:@"0"]) {
+            return 280;
+        }
+        else {
+             return 245;
+        }
+       
+    }else {
+        return 95;
+    }    
 }
 
 - (void)didReceiveMemoryWarning
